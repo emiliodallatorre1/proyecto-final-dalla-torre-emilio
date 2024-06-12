ee91053c240bc22f4aade165cda6e71f6c384e4b
Emilio Matías Dalla Torre <emiliodallatorre1996@gmail.com>
2024-06-12 11:33:59 -0300

    carga de models

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,11 @@
+# aplicacion/models.py
+
 from django.db import models
 
-# Create your models here.
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=20)
+    edad = models.IntegerField()
+    correo = models.EmailField()
+
+    def __str__(self):
+        return self.nombre
