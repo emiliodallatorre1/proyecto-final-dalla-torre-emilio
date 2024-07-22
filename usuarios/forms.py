55e580c6accf51755bd2378ba0a21953b81c63e8
Emilio Matías Dalla Torre <emiliodallatorre1996@gmail.com>
2024-07-22 18:47:38 -0300

    aplicación de las migraciones necesarias

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,18 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm as DjangoPasswordChangeForm
-from django.contrib.auth.models import User
+from .models import Usuario
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
-        model = User
+        model = Usuario
         fields = ['username', 'email', 'password1', 'password2']
 
 class UserEditForm(UserChangeForm):
     class Meta:
-        model = User
+        model = Usuario
         fields = ['username', 'email']
 
 class PasswordChangeForm(DjangoPasswordChangeForm):
     class Meta:
-        model = User
+        model = Usuario
         fields = ['old_password', 'new_password1', 'new_password2']
